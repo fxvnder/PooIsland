a004e2a5356dfb5a7707c5f5f1e720858de58859
founder <djjotaremix@gmail.com>
2021-11-12 18:06:31 +0000

    doin sumn
    ya

---
+++ cmake-build-debug/CMakeFiles/PooIsland.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PooIsland.dir/main.cpp.obj"
+  "PooIsland.exe"
+  "PooIsland.exe.manifest"
+  "PooIsland.pdb"
+  "libPooIsland.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PooIsland.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
