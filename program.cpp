60418bb1ab01789329e599b88d58de3718690d6d
Yeshey <41551785+Yeshey@users.noreply.github.com>
2021-11-15 02:02:04 +0000

    p

--- program.cpp
+++ program.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "program.h"
 
+tiles::tiles() : type("forest") {};
+
+island::island(int l,int c) : lines(l), columns(c), vec(lines,42) {};
+string island::showGuts() const {
+    ostringstream oss;
+    oss << "lets go " << vec[0];
+    return oss.str();
+};
+
 int run(){
-    std::cout << "Hello world" << std::endl;
     island world(4,4);
+    std::cout << world.showGuts() << std::endl;
     return 0;
 }
--- program.h
+++ program.h
@@ -1,18 +1,27 @@
 #ifndef POOISLAND_PROGRAM_H
 #define POOISLAND_PROGRAM_H
 #include <vector>
+#include <sstream>
 using std::vector;
+using std::string;
+using std::ostringstream;
+
+class tiles{
+private:
+    string type;
+public:
+    tiles();
+    string operator<< (const tiles& type) const {return "nope"; }; //?
+};
 
 class island{
 private:
     int lines, columns;
-    // vector<vector<int>> vect;
+    //vector<vector<tiles>> vec;
     vector<int> vec;
-
 public:
-    island(int l,int c) : lines(l), columns(c) {
-        
-    };
+    island(int l,int c);
+    string showGuts() const;
 };
 
 int run();
