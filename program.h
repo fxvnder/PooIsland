0f85cd62965d4b6d87188291e34d0770340107d9
Yeshey <41551785+Yeshey@users.noreply.github.com>
2021-11-17 01:26:46 +0000

    Done 2d vector

--- program.h
+++ program.h
@@ -11,7 +11,7 @@ public:
     tile() : type("forest") {};
     std::string showInfoTile() const {
         std::ostringstream oss;
-        oss << type << std::endl;
+        oss << type;
         return oss.str();
     };
     //std::string operator<< (const tile& type) const {return "nope"; };
@@ -21,21 +21,31 @@ class island{
 private:
     int lines, columns;
     std::vector<std::vector<tile>> vecvec;
-    std::vector<tile> vec;
+    std::vector<tile> vetr;
 public:
     island(int l,int c) : lines(l), columns(c) {
         for (int i = 0; i < l; ++i) {
+            vecvec.push_back(std::vector<tile>());
             for (int j = 0; j < c; ++j) {
-                tile moist;
-                vec.push_back(moist);
+                tile newTile;                      // todo: find a better way, maybe use new
+                vecvec[i].push_back(newTile);
             }
         }
     };
     std::string showInfoIsland() const {
         std::ostringstream oss;
         oss << "Showing Island Info" << std::endl;
-        for (tile i : vec)
-            oss << i.showInfoTile();
+        /*
+        for (std::vector<int> vec : vecvec)
+            for (int x : vec)
+                oss << x;
+        */
+        for (int i = 0; i < vecvec.size(); i++){
+            for (int j = 0; j < vecvec[i].size(); j++){
+                oss << vecvec[i][j].showInfoTile() << " ";
+            }
+            oss << std::endl;
+        }
         oss << std::endl;
         return oss.str();
     };
