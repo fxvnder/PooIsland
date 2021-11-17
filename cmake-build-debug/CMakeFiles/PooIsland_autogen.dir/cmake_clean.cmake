4c5a644806b061f314ddd5f8f97eb6d7bc56cb75
FXVNDER <a21280282@isec.pt>
2021-11-17 18:46:49 +0000

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/PooIsland_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PooIsland_autogen"
-  "PooIsland_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PooIsland_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
