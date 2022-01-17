071bf9317e99490786af950016ab3b99954b94ce
Yeshey <41551785+Yeshey@users.noreply.github.com>
2022-01-17 19:57:03 +0000

    removed unused qt - final commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 // #include <QCoreApplication>
 // #include <QDebug>
 
-#include <src/interface.h>
+#include "src/interface.h"
 
 int main() {
-    //QCoreApplication a(argc, argv);
-    //qDebug() << "Hello World";
-    //return QCoreApplication::exec();
 
     gameData game_on; // starts gameData
     interface ui(game_on); // starts the interface
